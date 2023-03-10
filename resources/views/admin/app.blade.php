7caa97c60af72a67c7b67a2f5813f3a8157b5d26
RaiA133 <rai010303@gmail.com@gmail.com>
2023-03-10 13:56:30 +0700

    all link path fix (css & js)

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -12,12 +12,11 @@
   <title>@yield('title') | Dashboard</title>
 
   <!-- Custom fonts -->
-  <link href="vendoradmin/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="{{ asset ('vendoradmin/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles -->
   <link href="{{ asset ('cssadmin/sb-admin-2.min.css') }}" rel="stylesheet" type="text/css">
-  <link href="cssadmin/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -38,21 +37,21 @@
     </a>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendoradmin/jquery/jquery.min.js"></script>
-    <script src="vendoradmin/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset ('vendoradmin/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset ('vendoradmin/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendoradmin/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{ asset ('vendoradmin/jquery-easing/jquery.easing.min.js') }}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="jsadmin/sb-admin-2.min.js"></script>
+    <script src="{{ asset ('jsadmin/sb-admin-2.min.js') }}"></script>
 
     <!-- Page level plugins -->
-    <script src="vendoradmin/chart.js/Chart.min.js"></script>
+    <script src="{{ asset ('vendoradmin/chart.js/Chart.min.js') }}"></script>
 
     <!-- Page level custom scripts -->
-    <script src="jsadmin/demo/chart-area-demo.js"></script>
-    <script src="jsadmin/demo/chart-pie-demo.js"></script>
+    <script src="{{ asset ('jsadmin/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset ('jsadmin/demo/chart-pie-demo.js') }}"></script>
 
 </body>
 
