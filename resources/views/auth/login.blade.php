d4701126df0c03c1de5b6ff2eac11c168364929c
rifqimunawar <rifqimunawar48@gmail.com>
2023-03-13 18:04:28 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,16 @@
 			<input type="password" name="password" class="password" placeholder="Password"><br>
       <button type="submit" class="submit">Login</button>
 		</div>
+    @if (session('error'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert" style="background-color: red; heigt:2rem; padding:10px; border-radius;10px">
+        {{ session('error') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+  @endif
   </form>
+
 
 	
 
